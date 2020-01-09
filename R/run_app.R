8b7aa7ea569cad4a38a9ce3c6d8a5a96458647fa
Tristan Ganry <tristanganry@gmail.com>
2020-01-09 15:28:39 -0500

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -13,8 +13,26 @@ run_app <- function(...) {
   library(dashHtmlComponents)
   library(sf)
   library(data.table)
+  library(plotly)
+  library(dashHtmlComponents)
+  library(dashCoreComponents)
+  library(rmapshaper)
   
   map_data <- fread("/usr/local/data/DataForMaps.csv", colClasses = c(GEOID10 = "character"))
+  print(map_data[1])
+  
+  mapbox_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNqdnBvNDMyaTAxYzkzeW5ubWdpZ2VjbmMifQ.TXcBE-xg9BFdV2ocecc_7g"
+  Sys.setenv('MAPBOX_TOKEN' = mapbox_token)
+  
+  shapefile <- st_read("/usr/local/data/shapes/COI20.shp")
+
+  merged <- merge(map_data, shapefile, by="GEOID10")
+  st_geometry(merged) <- merged$geometry
+  st_crs(merged) <- 4326
+
+  sequential_colorscale <- c("#013457", "#045781", "#2E74B5", "#52C1FA", "#B5E5FD")
+
+  
   app <- Dash$new()
   
   app$layout(app_ui())
